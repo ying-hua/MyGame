349013d0edbc3e157516bfc641c93d34a5f125aa
ying-hua <60057611+ying-hua@users.noreply.github.com>
2020-06-03 15:06:40 +0800

    方块类函数的实现

---
+++ block.cpp
@@ -0,0 +1,48 @@
+#pragma once
+#define BLUE FOREGROUND_BLUE
+#define GREEN FOREGROUND_GREEN
+#define RED FOREGROUND_RED
+#define INTENSITY FOREGROUND_INTENSITY
+#include<time.h>
+#include<stdlib.h>
+#include"block.h"
+#include"player.h"
+Block::Block() {
+	posX = 0;
+	posY = 0;
+	color = 0;
+	shape = 0;
+}
+void Block::setXY(int x, int y) {
+	posX = x;
+	posY = y;
+}
+void Block::roundBlock() {
+	int random;//随机数
+	srand((unsigned)time(NULL));
+	random = rand() % 7;//产生0-6的随机数,随机一种方块
+	random = random * 4 + 1;
+	switch (random) {//随机方块形状和颜色
+		case 1:color = GREEN | INTENSITY; random += rand() % 2; break;//亮绿
+		case 5:color = RED | INTENSITY;break;//红
+		case 9:color = RED | BLUE | INTENSITY; random += rand() % 4; break;//品红
+		case 13:color = RED | GREEN | INTENSITY; random += rand() % 4; break;//黄
+		case 17:color = BLUE | GREEN | INTENSITY; random += rand() % 4; break;//青
+		case 21:color = BLUE | INTENSITY; random += rand() % 2; break;//蓝
+		case 25:color = GREEN; random += rand() % 2; break;//绿
+		default:break;
+	}
+	shape = random;
+}
+int Block::getX() {
+	return posX;
+}
+int Block::getY() {
+	return posY;
+}
+int Block::getColor() {
+	return color;
+}
+int Block::getShape() {
+	return shape;
+}
