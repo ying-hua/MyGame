26ac0a81d65cd5b423d0012d249944621978c2a3
ying-hua <60057611+ying-hua@users.noreply.github.com>
2020-06-05 15:45:46 +0800

    稍做修改

--- render.h
+++ render.h
@@ -1,14 +1,15 @@
 #pragma once
 #include<windows.h>
 #include<string>
+#include"player.h"
 using namespace std;
 class Render {
 public:
-	void initialPrint1(HANDLE hOut);  //初始化单人界面
-	void initialPrint2(HANDLE hOut);//初始化双人界面
-	void gotoXY(HANDLE hOut, int x, int y);  //移动光标
-	void printBlock(HANDLE hOut, int block[4][4], int x, int y,int color);//打印方块
-	void clearBlock(HANDLE hOut, int block[4][4], int x, int y);//消除方块
-	void printName(HANDLE hOut, string name, int x, int y);//打印玩家名字
-	void printScore(HANDLE hOut, int score, int x, int y);//更新分数
+	static void initialPrint1(HANDLE hOut);  //初始化单人界面
+	static void initialPrint2(HANDLE hOut);//初始化双人界面
+	static void gotoXY(HANDLE hOut, int x, int y);  //移动光标
+	static void printBlock(HANDLE hOut, int block[4][4],int x,int y,int color);//打印方块
+	static void clearBlock(HANDLE hOut, int block[4][4],int x,int y);//消除方块
+	static void printScore(HANDLE hOut, int score, int x, int y);//更新分数
+	static void printMap(HANDLE hOut,Player player);
 };
