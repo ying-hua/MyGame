a0a850fec897f589cbf45c1b623e47a73a534482
ying-hua <60057611+ying-hua@users.noreply.github.com>
2020-06-05 22:39:42 +0800

    修复bug

--- render.h
+++ render.h
@@ -5,8 +5,8 @@
 using namespace std;
 class Render {
 public:
-	static void initialPrint1(HANDLE hOut);  //初始化单人界面
-	static void initialPrint2(HANDLE hOut);//初始化双人界面
+	static void initialPrint1(HANDLE hOut,string name);  //初始化单人界面
+	static void initialPrint2(HANDLE hOut,string name1,string name2);//初始化双人界面
 	static void gotoXY(HANDLE hOut, int x, int y);  //移动光标
 	static void printBlock(HANDLE hOut, int block[4][4],int x,int y,int color);//打印方块
 	static void clearBlock(HANDLE hOut, int block[4][4],int x,int y);//消除方块
