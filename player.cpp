539a8418f34ebf04f4e5c24b59b6694be3542b3e
ying-hua <60057611+ying-hua@users.noreply.github.com>
2020-06-05 22:40:47 +0800

    修复bug

--- player.cpp
+++ player.cpp
@@ -1,14 +1,20 @@
 #pragma once
+#define BLUE FOREGROUND_BLUE
+#define GREEN FOREGROUND_GREEN
+#define RED FOREGROUND_RED
+#define INTENSITY FOREGROUND_INTENSITY
 #include<string.h>
 #include<time.h>
 #include"player.h"
 #include"block.h"
 #include"render.h"
 Player::Player() :nextBlock(),nowBlock(){
-	name = "PlayerA";
+	name = "Player";
 	score = 0;
 	memset(map, 0, sizeof(map));
 	mapX = 0;
+	nextBlock.roundBlock();
+	nowBlock.roundBlock();
 }
 void Player::setName(string n) {
 	name = n;
@@ -25,7 +31,7 @@ string Player::getName() {
 int Player::getScore() {
 	return score;
 }
-int Player::eliminateRow() { //从下往上消行
+int Player::eliminateRow(HANDLE hOut) { //从下往上消行
 	int cnt = 0;
 	int flag = 1; //某行全为1则flag=1
 	for (int i = 20; i >= 1; ) {
@@ -48,6 +54,8 @@ int Player::eliminateRow() { //从下往上消行
 			}
 		}
 	}
+	SetConsoleTextAttribute(hOut, RED | GREEN | BLUE);
+	Render::printScore(hOut, score, 36 + mapX, 3); //在指定位置打印分数,玩家1在(36,3),玩家2在(86,3)
 	return cnt;
 }
 
@@ -66,7 +74,7 @@ void Player::addRow(int x) { //增加x行
 			}
 		}
 		for (int i = 2; i <= 20; i++) { //地图上移
-			for (int j = 1; i <= 10; j++) {
+			for (int j = 1; j <= 10; j++) {
 				map[i - 1][j] = map[i][j];
 			}
 		}
@@ -110,7 +118,7 @@ void Player::goLeft(HANDLE hOut) {
 		Render::clearBlock(hOut, stdblock[shape], 2 * y + mapX, x - 1);
 		y--;
 		nowBlock.setY(y);
-		Render::printBlock(hOut, stdblock[shape], 2 * y + mapX, x - 1, color);//可以去掉?
+		//Render::printBlock(hOut, stdblock[shape], 2 * y + mapX, x - 1, color);//可以去掉?
 	}
 }
 void Player::goRight(HANDLE hOut) {
@@ -126,7 +134,7 @@ void Player::goRight(HANDLE hOut) {
 		Render::clearBlock(hOut, stdblock[shape], 2 * y + mapX, x - 1);
 		y++;
 		nowBlock.setY(y);
-		Render::printBlock(hOut, stdblock[shape], 2 * y + mapX, x - 1, color);//去掉？
+		//Render::printBlock(hOut, stdblock[shape], 2 * y + mapX, x - 1, color);//去掉？
 	}
 }
 void Player::buildMap(int x,int y,int shape) { //将方块固定在地图中
@@ -157,7 +165,7 @@ void Player::goDown(HANDLE hOut,Player opponent) { //下落
 	}
 	else { //无法下落
 		buildMap(x,y,shape);//将方块固定在地图中
-		cnt = eliminateRow();
+		cnt = eliminateRow(hOut);
 		if (cnt!=0) { //可以消行
 			opponent.addRow(cnt);
 			Render::printMap(hOut, opponent);//更新对手的地图
@@ -166,12 +174,14 @@ void Player::goDown(HANDLE hOut,Player opponent) { //下落
 		nowBlock = nextBlock;
 		nowBlock.setX(-2);//方块从地图上方落下
 		nowBlock.setY(4);
+		Render::clearBlock(hOut, stdblock[nextBlock.getShape()], 30 + mapX, 6);//清除下一个方块
 		nextBlock.roundBlock(); //随机生成下一个方块
 		x = nowBlock.getX();//变量更新
 		y = nowBlock.getY();
 		shape = nowBlock.getShape();
 		color = nowBlock.getColor();
-		Render::printBlock(hOut, stdblock[shape], 2 * y + mapX, x - 1, color); //打印刚落下的方块
+		//Render::printBlock(hOut, stdblock[shape], 2 * y + mapX, x - 1, color); //打印刚落下的方块
+		Render::printBlock(hOut, stdblock[nextBlock.getShape()], 30 + mapX, 6, nextBlock.getColor());//打印下一个方块
 	}
 }
 void Player::transform(HANDLE hOut) {
@@ -187,27 +197,27 @@ void Player::transform(HANDLE hOut) {
 	if (shape == 2) {
 		tempBlock.setShape(1);
 		if (!collisionDetection(tempBlock)) {
-			Render::clearBlock(hOut, stdblock[shape], x, y);
+			Render::clearBlock(hOut, stdblock[shape], 2 * y + mapX, x - 1);
 			nowBlock.setShape(1);
 			return;
 		}
 		tempBlock.setY(y - 1);
 		if (!collisionDetection(tempBlock)) {
-			Render::clearBlock(hOut, stdblock[shape], x, y);
+			Render::clearBlock(hOut, stdblock[shape], 2 * y + mapX, x - 1);
 			nowBlock.setShape(1);
 			nowBlock.setY(y - 1);
 			return;
 		}
 		tempBlock.setY(y + 1);
 		if (!collisionDetection(tempBlock)) {
-			Render::clearBlock(hOut, stdblock[shape], x, y);
+			Render::clearBlock(hOut, stdblock[shape], 2 * y + mapX, x - 1);
 			nowBlock.setShape(1);
 			nowBlock.setY(y + 1);
 			return;
 		}
 		tempBlock.setY(y + 2);
 		if (!collisionDetection(tempBlock)) {
-			Render::clearBlock(hOut, stdblock[shape], x, y);
+			Render::clearBlock(hOut, stdblock[shape], 2 * y + mapX, x - 1);
 			nowBlock.setShape(1);
 			nowBlock.setY(y + 2);
 			return;
@@ -216,7 +226,7 @@ void Player::transform(HANDLE hOut) {
 	else if (shape % 2 == 1) {
 		tempBlock.setShape(shape + 1);
 		if (!collisionDetection(tempBlock)) {
-			Render::clearBlock(hOut, stdblock[shape], x, y);
+			Render::clearBlock(hOut, stdblock[shape], 2 * y + mapX, x - 1);
 			nowBlock.setShape(shape + 1);
 			return;
 		}
@@ -224,13 +234,13 @@ void Player::transform(HANDLE hOut) {
 	else if (shape % 4 == 0) {
 		tempBlock.setShape(shape - 3);
 		if (!collisionDetection(tempBlock)) {
-			Render::clearBlock(hOut, stdblock[shape], x, y);
+			Render::clearBlock(hOut, stdblock[shape], 2 * y + mapX, x - 1);
 			nowBlock.setShape(shape - 3);
 			return;
 		}
 		tempBlock.setY(y - 1);
 		if (!collisionDetection(tempBlock)) {
-			Render::clearBlock(hOut, stdblock[shape], x, y);
+			Render::clearBlock(hOut, stdblock[shape], 2 * y + mapX, x - 1);
 			nowBlock.setShape(shape - 3);
 			nowBlock.setY(y - 1);
 			return;
@@ -244,13 +254,13 @@ void Player::transform(HANDLE hOut) {
 			tshape = shape - 1;
 		tempBlock.setShape(tshape);
 		if (!collisionDetection(tempBlock)) {
-			Render::clearBlock(hOut, stdblock[shape], x, y);
+			Render::clearBlock(hOut, stdblock[shape], 2 * y + mapX, x - 1);
 			nowBlock.setShape(tshape);
 			return;
 		}
 		tempBlock.setY(y + 1);
 		if (!collisionDetection(tempBlock)) {
-			Render::clearBlock(hOut, stdblock[shape], x, y);
+			Render::clearBlock(hOut, stdblock[shape], 2 * y + mapX, x - 1);
 			nowBlock.setShape(tshape);
 			nowBlock.setY(y + 1);
 			return;
