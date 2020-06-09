8cff139e60721cc72222e56a8898be0ef89948d7
ying-hua <60057611+ying-hua@users.noreply.github.com>
2020-06-09 22:08:14 +0800

    V1.0

--- block.cpp
+++ block.cpp
@@ -1,3 +1,6 @@
+/*===========================
+该文件实现了方块类的成员函数
+=============================*/
 #pragma once
 #define BLUE FOREGROUND_BLUE
 #define GREEN FOREGROUND_GREEN
@@ -7,30 +10,42 @@
 #include<stdlib.h>
 #include"block.h"
 #include"player.h"
+//构造函数
 Block::Block() {
 	posX = 0;
 	posY = 0;
 	color = 0;
 	shape = 0;
 }
+//复制构造函数
 Block::Block(const Block& b2) {
 	posX = b2.posX;
 	posY = b2.posY;
 	color = b2.color;
 	shape = b2.shape;
 }
+//设置方块x坐标
 void Block::setX(int x) {
 	posX = x;
 }
+//设置方块y坐标
 void Block::setY(int y) {
 	posY = y;
 }
+//设置方块形状
 void Block::setShape(int s) {
 	shape = s;
 }
+/*===========================
+随机生成方块
+先随机方块的类型(7种类型之一)
+若该类型的方块有不同变化形状  (如横条和竖条就是一种类型的两种变化形状)
+则再随机一种变化形状
+一种类型的方块始终对应一种颜色
+最后存入方块的shape和color属性
+=============================*/
 void Block::roundBlock() {
 	int random;//随机数
-	//srand((unsigned)time(NULL));
 	random = rand() % 7;//产生0-6的随机数,随机一种方块
 	random = random * 4 + 1;
 	switch (random) {//随机方块形状和颜色
@@ -45,15 +60,19 @@ void Block::roundBlock() {
 	}
 	shape = random;
 }
+//获取方块x坐标
 int Block::getX() {
 	return posX;
 }
+//获取方块y坐标
 int Block::getY() {
 	return posY;
 }
+//获取方块颜色
 int Block::getColor() {
 	return color;
 }
+//获取方块形状
 int Block::getShape() {
 	return shape;
 }
