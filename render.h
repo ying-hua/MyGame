abfa9ad0d62f1b9816f5916423040e1be501c6d3
ying-hua <60057611+ying-hua@users.noreply.github.com>
2020-06-09 22:12:45 +0800

    V1.0

--- render.h
+++ render.h
@@ -1,3 +1,13 @@
+/*===========================
+该文件定义了渲染类，用于控制渲染，即将游戏内容打印在屏幕上
+单人模式和双人模式下游戏界面的初始化有所不同
+因此分别定义了函数
+关于坐标的不同：
+渲染类的成员函数需要传入的坐标参数都为方块在屏幕上的坐标
+而不是在玩家地图中的坐标
+因此该坐标与方块类中的坐标不同
+方块坐标需要转换成屏幕上的坐标再传参
+=============================*/
 #pragma once
 #include<windows.h>
 #include<string>
@@ -11,5 +21,6 @@ public:
 	static void printBlock(HANDLE hOut, int block[4][4],int x,int y,int color);//打印方块
 	static void clearBlock(HANDLE hOut, int block[4][4],int x,int y);//消除方块
 	static void printScore(HANDLE hOut, int score, int x, int y);//更新分数
-	static void printMap(HANDLE hOut,Player player);
+	static void printMap(HANDLE hOut, Player player);//打印玩家地图
+	static void printWin(HANDLE hOut, string name);//双人模式玩家获胜画面
 };
